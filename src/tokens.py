262507c25a98ecc94335ad488b07907d522e01a8
stackdynamic <timothy.tm.zhou@gmail.com>
2020-02-23 22:50:19 -0800

    refactor tokens.py

--- src/tokens.py
+++ src/tokens.py
@@ -46,19 +46,6 @@ operator_precedence = (
 )
 
 
-class Token:
-    def __init__(self, token_type, value, node_type=None):
-        self.token_type = token_type
-        self.value = value
-        self.node_type = node_type
-
-    def __str__(self):
-        string = f"{self.token_type}: {self.value}"
-        if self.node_type is not None:
-            string += f", {self.node_type.__name__}"
-        return string
-
-
 class TokenType(Enum):
     UNARY = 1
     BINARY = 2
@@ -71,21 +58,31 @@ class TokenType(Enum):
     SEPARATOR = 9
 
 
+class Token:
+    def __init__(self, value, token_type=None, node_type=None):
+        if token_type == None:
+            token_type = token_types[value]
+        self.token_type = token_type
+        self.value = value
+        self.node_type = node_type
+
+    def __str__(self):
+        string = f"{self.token_type}: {self.value}"
+        if self.node_type is not None:
+            string += f", {self.node_type.__name__}"
+        return string
+
+
 # token types initialization
-token_types = {}
-
-
-def add_tokens(token_type, tokens):
-    token_types.update({token: token_type for token in tokens})
-
-
-add_tokens(TokenType.UNARY, ("!", "++", "--"))
-add_tokens(TokenType.BINARY, tuple(binary_operators))
-add_tokens(TokenType.CONTROL_FLOW, ("if", "elif", "else", "while", "for", "do"))
-add_tokens(TokenType.KEYWORD, ("print",))
-add_tokens(
-    TokenType.SYMBOL,
-    (":", "=>", "=", "+=", "-=", "*=", "/=", "**=", "%=", "@=", ">>=", "<<=", "&=", "|=", "^=")
-)
-add_tokens(TokenType.GROUPING, ("(", ")", "{", "}", ";"))
-add_tokens(TokenType.SEPARATOR, (" ", ","))
+token_types = {
+    token: token_type for token_type, tokens in (
+        (TokenType.UNARY, ("!", "++", "--")),
+        (TokenType.BINARY, tuple(binary_operators)),
+        (TokenType.CONTROL_FLOW, ("if", "elif", "else", "while", "for", "do")),
+        (TokenType.KEYWORD, ("print",)),
+        (TokenType.SYMBOL, (":", "=>", "=", "+=", "-=", "*=", "/=", "**=", "%=", "@=", ">>=",
+                            "<<=", "&=", "|=", "^=")),
+        (TokenType.GROUPING, ("(", ")", "{", "}", ";")),
+        (TokenType.SEPARATOR, (" ", ","))
+    ) for token in tokens
+}
